5a3f53991f1c621d1f414036afd1110297b05829
Pia <piag.velasquez@outlook.com>
2024-10-14 01:26:15 -0300

    added  component TimmerInput and SectionTimmerInput

---
+++ src/components/TimerInput/TimerInput.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const TimerInput = ({name, defaultTime}) => {
+  return (
+    <div className='timerInput__container'>
+        <p>{name}</p>
+        <button className="timerInput_button_increment"/>
+        <div className='timerInput_show_time'>
+            <span >{defaultTime}</span>
+        </div>
+        <button className="timerInput_button_decrement"/>
+        <button className="timerInput_restart">Reiniciar</button>
+    </div>
+  )
+}
+
+TimerInput.propTypes = {}
+
+export default TimerInput
